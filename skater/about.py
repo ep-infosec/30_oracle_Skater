c48b6d6a60c880f2aadf348f295b1340185bf220
ep-infosec <ep-infosec@users.noreply.github.com>
2022-12-10 12:07:08 +0200

    init

---
+++ skater/about.py
@@ -0,0 +1,11 @@
+# Reference: http://python-packaging.readthedocs.io/en/latest/dependencies.html
+
+__title__ = 'skater'
+__version__ = '1.1.2b1'
+__summary__ = 'Model Interpretation Library'
+__uri__ = 'https://github.com/datascienceinc/skater/'
+__author__ = 'Oracle, and contributors'
+__maintainer__ = 'Oracle community'
+__email__ = 'skater-users@googlegroups.com'
+__license__ = 'Oracle UPL'
+__docs__ = 'https://oracle.com'
